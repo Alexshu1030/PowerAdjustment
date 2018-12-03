4d45b5d49ff6b43a0d83251075964fa25ada7443
jtanuan@research.baycrest.org <jtanuan@kakapo.rotman-baycrest.on.ca>
2018-12-03 14:31:03 -0500

    Linear Adjustment comparisson

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ grades <- as.numeric(unlist(read.csv("grades")))
 
 ui <- fluidPage(
    
-   titlePanel("Power Adjustment"),
+   titlePanel("Power vs Linear Adjustment"),
    sidebarLayout(
       sidebarPanel(
          sliderInput("power",
@@ -12,27 +12,39 @@ ui <- fluidPage(
                      min = 0,
                      max = 1,
                      value = 1, step = 0.1),
+         sliderInput("linAdd",
+                     "Linear Adjustment",
+                     min = 0,
+                     max = 100,
+                     value = 0,
+                     step = 10),
          sliderInput("num_breaks",
                      "Number of Breaks",
                      min = 1,
                      max = 50,
                      value = 20,
                      step = 1)
-      
          ),
 
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("distPlot"),
+         
+         plotOutput("linPlot")
       )
    )
 )
 
 server <- function(input, output) {
+  
+  output$linPlot <- renderPlot({
+    finalMark <- input$linAdd + grades
+    hist(finalMark, breaks = input$num_breaks, xlab= "Mark %", main = "Historgram of Grades after Linear Adjustment")
+  })
 
    output$distPlot <- renderPlot({
       numerator <- grades/100
       finalMark <- (numerator)^input$power*100
-      hist(finalMark, breaks = input$num_breaks, xlab = "Mark %", main = "Histogram of Grades after adjustment")
+      hist(finalMark, breaks = input$num_breaks, xlab = "Mark %", main = "Histogram of Grades after Poewr Adjustment")
    })
 }
 
